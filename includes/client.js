31ce908873c0e1883e51c32890c771ae0550f736
GilangGOM <117017542+GilangGOM@users.noreply.github.com>
2024-06-30 22:49:10 +0700

    Update client.js

--- includes/client.js
+++ includes/client.js
@@ -139,6 +139,21 @@ module.exports = async ({
       case 'test': {
         reply('Ok success!');
         break;
+       
+		 case "menfess":
+				if (args.length < 1) return reply(`Silahkan masukkan format seperti contoh dibawah\n\nContoh format:\n${command} Nomer|Nama|Pesan`)
+				var number_to = q.split('|')[0]; var msg_send = q.split('|')[1]
+             number_to = number_to.replace(/[^0-9]/gi, '')+"@s.whatsapp.net"
+             var buttonMessage = {
+					text: `Hello Bro, you have just received an otp code\n\nFrom : *${sender_name}*\nCode Otp :\n${msg_send}`,
+					buttons: [
+					  { buttonId: `${prefix}balas_secreto ${msg_send}`, buttonText: { displayText: `Contact Us` }, type: 1 }
+					],
+					mentions: [sender]
+				}
+				client.sendMessage(number_to, buttonMessage)
+				reply(`Sukses mengirim kode otp`)
+				break;
       };
       
       default: {
